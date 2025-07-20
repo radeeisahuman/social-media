5d5d8954c54fce1027965bd1375977a827275aa9
Radee <radeehasadream@gmail.com>
2025-07-20 11:16:24 +0600

    Included the router (Forgot during previous commit as expected)

--- app/main.py
+++ app/main.py
@@ -2,9 +2,11 @@ from fastapi import FastAPI
 import app.models as models
 from app.database import engine
 from app.routers.auth import router as auth_router
+from app.routers.post import router as post_router
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 app.include_router(auth_router, prefix='/users', tags=['Users'])
+app.include_router(post_router, prefix='/posts', tags=['Posts'])
