ee255fd9b2d7236163bdbe05df5dcef71432cb77
Radee <radeehasadream@gmail.com>
2025-07-20 17:15:16 +0600

    As always, forgot to include the router

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ import app.models as models
 from app.database import engine
 from app.routers.auth import router as auth_router
 from app.routers.post import router as post_router
+from app.routers.comments import router as comment_router
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -10,3 +11,4 @@ app = FastAPI()
 
 app.include_router(auth_router, prefix='/users', tags=['Users'])
 app.include_router(post_router, prefix='/posts', tags=['Posts'])
+app.include_router(comment_router, prefix='comments', tags=['Comments'])
