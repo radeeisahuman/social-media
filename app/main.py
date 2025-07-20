e48d584cea4129a84acbe9c12c3ca0a4053724c7
Radee <radeehasadream@gmail.com>
2025-07-20 09:01:42 +0600

    Registered router in main

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 import app.models as models
 from app.database import engine
+from app.routers.auth import router as auth_router
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.include_router(auth_router, prefix='/users', tags=['Users'])
